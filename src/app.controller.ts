4a2bdc62811b48014b2af311b9c634ae9629e1ea
created-by-pavel <pavelzavalnyuk11@gmail.com>
2024-07-09 20:47:36 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
-  @Render('index2')
+  @Render('index')
   root() {
     return;
   }
